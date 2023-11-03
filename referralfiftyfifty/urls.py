54b1c12879e5e0897268f69022a06fcb102de82c
daridev <darideveloper@gmail.com>
2023-11-03 12:36:09 -0600

    feat (users): main paths

--- referralfiftyfifty/urls.py
+++ referralfiftyfifty/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from apps.users import urls as users_urls
-from apps.users import views
 
 admin.site.site_header = "Referral Fifty Fifty"
 admin.site.site_title = 'Referral Fifty Fifty'
@@ -9,7 +8,6 @@ admin.site.site_url = '/'
 admin.site.index_title = "Referral Fifty Fifty"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(users_urls), name='users'),
     path('admin/', admin.site.urls),
-    path('users/', include(users_urls), name='users'),
 ]
